2c04a153614d70581e91202452514bb75fb6840a
Niek Vandael <niek.vandael@gmail.com>
2017-03-23 19:39:37 +0100

    Testing the app

--- src/GreenLiving/wwwroot/app/home/welcome.component.js
+++ src/GreenLiving/wwwroot/app/home/welcome.component.js
@@ -8,7 +8,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var WelcomeComponent = (function () {
     function WelcomeComponent() {
-        this.pageTitle = 'Welcome';
+        this.pageTitle = 'Prevention Advisor';
     }
     return WelcomeComponent;
 }());
