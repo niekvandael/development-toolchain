0697b7f811f190b6f40f9c988630734889c67424
Niek Vandael <niek.vandael@gmail.com>
2017-04-15 12:51:16 +0200

    Prod mode

--- src/PreventionAdvisor/wwwroot/app/main.ts
+++ src/PreventionAdvisor/wwwroot/app/main.ts
@@ -1,5 +1,10 @@
 // main entry point
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
+import { enableProdMode } from '@angular/core';
+
+if (location.protocol === 'https:') {
+    enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
